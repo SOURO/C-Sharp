fa5d7b9a6f8668a91ee6758a47c29d3cbef438f4
Souro <soura1990@gmail.com>
2015-05-19 14:55:45 +0530

    Rename File

---
+++ MvcApplicationDemo-SimpleView_Step2/MvcApplication1/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        [NonAction]
+        public string GetMessage()
+        {
+            return "I am learning C#, beleive me it is fun";
+        }
+        public ActionResult GetView()
+        {
+            return View("SampleView");
+
+        }
+    }
+}
